544f25e5ebef61e5623c70276d6d3060753c927d
Oliver Kiessler <kiessler@inceedo.com>
2016-10-07 11:51:11 -0700

    SPAYM-288 - Http client with proxy

--- lib/rpush/daemon/dispatcher/http.rb
+++ lib/rpush/daemon/dispatcher/http.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 module Rpush
   module Daemon
     module Dispatcher
@@ -5,7 +7,15 @@ module Rpush
         def initialize(app, delivery_class, _options = {})
           @app = app
           @delivery_class = delivery_class
-          @http = Net::HTTP::Persistent.new('rpush')
+          if defined?(Rails) and Rails.env.production? and defined?(Settings) and not Settings[:staging] and ENV['USE_HTTP_PROXY'] and ENV['USE_HTTP_PROXY'] == 'true'
+            ENV['http_proxy'] = "http://#{Settings[:proxy_host]}:#{Settings[:proxy_port]}"
+            ENV['HTTP_PROXY'] = ENV['http_proxy']
+            proxy = URI "http://#{Settings[:proxy_host]}:#{Settings[:proxy_port]}"
+            puts "Setting production http proxy to: #{ENV['http_proxy']}"
+            @http = Net::HTTP::Persistent.new('rpush', proxy)
+          else
+            @http = Net::HTTP::Persistent.new('rpush')
+          end
         end
 
         def dispatch(payload)
